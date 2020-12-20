a84ebde67e5cf66db2dde9d3b2657e1daeb165ab
code-shoot <franksmithattheshoot@gmail.com>
2020-12-20 17:21:06 +0530

    adding logo

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import { ReactComponent as Logo} from '../../assets/crown.svg'
+
+const Header = () => (
+  <div className="header">
+    <Link to='/' className="logo-container">
+      <Logo className="logo"/>
+    </Link>
+    <div className="options">
+      
+    </div>
+  </div>
+)

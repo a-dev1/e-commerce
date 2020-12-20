2704b68833b5ca5b859f9ee3300e24b6a45f49c2
code-shoot <franksmithattheshoot@gmail.com>
2020-12-20 17:00:07 +0530

    updating the styles for shop page

--- src/components/preview-item/preview-item.component.jsx
+++ src/components/preview-item/preview-item.component.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
+import './preview-item.styles.sass'
 
 const Preview = ({ title, items }) => {
   return (
-    <div className="preview">
-      <h1>{title}</h1>
-      {
-        items.filter((item, index) => index < 4).map(({ name, id, imageUrl, price }) => {
-          return (
-            <div key={id}>
-              <p>{name}</p>
-              <h3>{price}</h3>
-            </div>
-          )
-        })
-      }
+    <div >
+      <h1 className="title">{title}</h1>
+      <div className="preview">
+        {
+          items.filter((item, index) => index < 4).map(({ name, id, imageUrl, price }) => {
+            return (
+              <div key={id} className="preview-item">
+                <div className='image'
+                  style={{
+                    backgroundImage: `url(${imageUrl})`
+                  }} />
+                <div className="preview-footer">
+                  <span className="name">{name}</span>
+                  <span className="price">{price}</span>
+                </div>
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   )
 }
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Directory from '../../components/directory/directory.component'
-import './homepage.styles.scss';
+import './homepage.styles.sass';
 
 const HomePage = (props) => {
     console.log('props from homepage:', props);

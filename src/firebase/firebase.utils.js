2819fc568f671ae64bf7e8be7f27d68ab59a3253
code-shoot <franksmithattheshoot@gmail.com>
2021-01-01 00:41:25 +0530

    adding firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,4 +2,12 @@ import firebase from 'firebase/app';
 import 'firebase/firestore'; //fore database
 import 'firebase/auth'; //for authentication 
 
-const config = {}                  
+const config = {
+  apiKey: "AIzaSyDNb96QUGrCFInsUa3ZyFgeYex9SIqOmM4",
+  authDomain: "e-commerce-db-70094.firebaseapp.com",
+  projectId: "e-commerce-db-70094",
+  storageBucket: "e-commerce-db-70094.appspot.com",
+  messagingSenderId: "63066293929",
+  appId: "1:63066293929:web:f3f6a7b4acf028c1a80bb8",
+  measurementId: "G-R3DHJXQXHG"
+};               
